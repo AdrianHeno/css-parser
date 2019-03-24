75d1c0af283243877cb2dfa2b234925109a54622
AdrianHeno <adrian.heno@gmail.com>
2019-03-24 18:27:44 +1100

    Will now sort properties alphabetically and output to a new file

--- css-lintter.php
+++ css-lintter.php
@@ -18,4 +18,11 @@ $phpCli->option('f')
 $parser = new CssParser();
 $parser->loadFile($argv[1]);
 $parsedCss = $parser->parse();
-var_dump($parser);
+//var_dump($parser->parsed['main']);
+foreach ($parser->parsed['main'] as $selector => $propertyArray) {
+    ksort($parser->parsed['main'][$selector]);
+}
+
+$css = $parser->glue();
+$sourcePath = explode('.css', $argv[1]);
+file_put_contents($sourcePath[0] . '_' . microtime() . '.css', $css);
